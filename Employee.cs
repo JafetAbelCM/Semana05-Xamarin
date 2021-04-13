ab83208e00653cf1ea27c54e51230bc86e81e9cc
JafetAbelCM <58614432+JafetAbelCM@users.noreply.github.com>
2021-04-12 23:42:37 -0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace ListViewExample
+{
+    public class Employee
+    {
+        public string DisplayName { get; set; }
+    }
+}
